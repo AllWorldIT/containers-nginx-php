427b84c3468fbff2e96c44f6c34720bc4e3d96d6
Nigel Kukard <nkukard@lbsd.net>
2021-02-27 14:47:56 +0000

    Fixed IonCube support

--- init.d/50-php.sh
+++ init.d/50-php.sh
@@ -2,5 +2,6 @@
 
 # Check if we need to enable ionCube
 if [ "$ENABLE_IONCUBE" == "yes" ]; then
+	echo "PHP: Enabling IONCUBE"
 	mv /etc/php7/conf.d/00_ioncube.ini.disabled /etc/php7/conf.d/00_ioncube.ini
 fi
