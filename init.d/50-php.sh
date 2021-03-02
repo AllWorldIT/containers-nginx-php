e0aaa583822b764b4c093c644d144be5716caf12
Nigel Kukard <nkukard@lbsd.net>
2021-03-02 12:58:29 +0000

    Added new option PHP_TIMEZONE

--- init.d/50-php.sh
+++ init.d/50-php.sh
@@ -5,3 +5,13 @@ if [ "$ENABLE_IONCUBE" == "yes" ]; then
 	echo "PHP: Enabling IONCUBE"
 	mv /etc/php7/conf.d/00_ioncube.ini.disabled /etc/php7/conf.d/00_ioncube.ini
 fi
+
+# Check if we have a timezone and set it
+if [ -n "$PHP_TIMEZONE" ]; then
+	cat <<EOF >> /etc/php7/conf.d/50-docker.ini
+
+[Date]
+date.timezone=${PHP_TIMEZONE}
+
+EOF
+
